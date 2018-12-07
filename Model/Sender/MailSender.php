8e26c36626d5f7a1bab49f7a6ea46fe28bacf8d2
Marcus Venghaus <info@inklammern.de>
2018-12-07 16:14:22 +0100

    admin from fix

--- Model/Sender/MailSender.php
+++ Model/Sender/MailSender.php
@@ -4,72 +4,78 @@ namespace Inkl\SystemMail\Model\Sender;
 
 use Magento\Framework\App\Area;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Mail\Template\TransportBuilderByStore;
 use Magento\Framework\Mail\Template\TransportBuilderFactory;
 use Magento\Store\Model\Store;
 
 class MailSender
 {
-    const TYPE_INFO = 'info';
-    const TYPE_WARNING = 'warning';
-    const TYPE_CRITICAL = 'critical';
+	const TYPE_INFO = 'info';
+	const TYPE_WARNING = 'warning';
+	const TYPE_CRITICAL = 'critical';
 
-    /** @var TransportBuilderFactory */
-    private $transportBuilderFactory;
-    /** @var ScopeConfigInterface */
-    private $scopeConfig;
+	/** @var TransportBuilderFactory */
+	private $transportBuilderFactory;
+	/** @var TransportBuilderByStore */
+	private $transportBuilderByStore;
+	/** @var ScopeConfigInterface */
+	private $scopeConfig;
 
-    /**
-     * @param TransportBuilderFactory $transportBuilderFactory
-     * @param ScopeConfigInterface $scopeConfig
-     */
-    public function __construct(TransportBuilderFactory $transportBuilderFactory,
-                                ScopeConfigInterface $scopeConfig)
-    {
-        $this->transportBuilderFactory = $transportBuilderFactory;
-        $this->scopeConfig = $scopeConfig;
-    }
+	/**
+	 * @param TransportBuilderFactory $transportBuilderFactory
+	 * @param ScopeConfigInterface $scopeConfig
+	 */
+	public function __construct(TransportBuilderFactory $transportBuilderFactory,
+	                            TransportBuilderByStore $transportBuilderByStore,
+	                            ScopeConfigInterface $scopeConfig)
+	{
+		$this->transportBuilderFactory = $transportBuilderFactory;
+		$this->transportBuilderByStore = $transportBuilderByStore;
+		$this->scopeConfig = $scopeConfig;
+	}
 
-    public function send($type, $subject, $body)
-    {
-        $toEmails = trim($this->scopeConfig->getValue(sprintf('inkl_systemmail/emails/%s', $type)));
-        if (!$toEmails)
-        {
-            return;
-        }
+	public function send($type, $subject, $body)
+	{
+		$toEmails = trim($this->scopeConfig->getValue(sprintf('inkl_systemmail/emails/%s', $type)));
+		if (!$toEmails)
+		{
+			return;
+		}
 
 		$toEmails = array_map('trim', explode("\n", $toEmails));
 
-        $transport = $this->transportBuilderFactory->create()
-            ->setTemplateOptions([
-                'area' => Area::AREA_ADMINHTML,
-                'store' => Store::DEFAULT_STORE_ID
-            ])
-            ->setTemplateIdentifier('system_mail')
-            ->setTemplateVars([
-                'type' => strtoupper($type),
-                'subject' => $subject,
-                'body' => $body
-            ])
-            ->setFrom('general')
-            ->addTo($toEmails)
-            ->getTransport();
+		$transport = $this->transportBuilderFactory->create()
+			->setTemplateOptions([
+				'area' => Area::AREA_ADMINHTML,
+				'store' => Store::DEFAULT_STORE_ID
+			])
+			->setTemplateIdentifier('system_mail')
+			->setTemplateVars([
+				'type' => strtoupper($type),
+				'subject' => $subject,
+				'body' => $body
+			])
+			->addTo($toEmails)
+			->getTransport();
 
-        $transport->sendMessage();
-    }
+		$this->transportBuilderByStore->setFromByStore('general', Store::DEFAULT_STORE_ID);
 
-    public function sendInfo($subject, $body)
-    {
-        $this->send(self::TYPE_INFO, $subject, $body);
-    }
+		$transport->sendMessage();
+	}
 
-    public function sendWarning($subject, $body)
-    {
-        $this->send(self::TYPE_WARNING, $subject, $body);
-    }
+	public function sendInfo($subject, $body)
+	{
+		$this->send(self::TYPE_INFO, $subject, $body);
+	}
 
-    public function sendCritical($subject, $body)
-    {
-        $this->send(self::TYPE_CRITICAL, $subject, $body);
-    }
+	public function sendWarning($subject, $body)
+	{
+		$this->send(self::TYPE_WARNING, $subject, $body);
+	}
+
+	public function sendCritical($subject, $body)
+	{
+		$this->send(self::TYPE_CRITICAL, $subject, $body);
+	}
 
 }
